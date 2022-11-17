a4882866260958128434cbebeb28a4730059dc84
Mark J Menger <mjmenger@users.noreply.github.com>
2022-11-17 17:48:04 +0000

    azure base networks 1 and 2

---
+++ modules/base-azure-network/dependency.tf
@@ -0,0 +1,15 @@
+output projectPrefix {
+  value = var.projectPrefix
+}
+output azureRegion {
+  value = var.azureRegion
+}
+output resourceGroup {
+  value = "${var.projectPrefix}-f5-xc"
+}
+output hubVnetName {
+  value = azurerm_virtual_network.f5-xc-hub.name
+}
+output workloadSubnet {
+  value = azurerm_subnet.workload-peer.id
+}
