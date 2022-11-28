30e81c5154dc98f6d40e2e124a43a8bbf964a9cd
Mark J Menger <mjmenger@users.noreply.github.com>
2022-11-28 17:21:06 +0000

    fix resource group name output to pass proper name

--- modules/base-azure-network/dependency.tf
+++ modules/base-azure-network/dependency.tf
@@ -5,7 +5,7 @@ output azureRegion {
   value = var.azureRegion
 }
 output resourceGroup {
-  value = "${var.projectPrefix}-f5-xc"
+  value =  azurerm_resource_group.rg.name
 }
 output hubVnetName {
   value = azurerm_virtual_network.f5-xc-hub.name
